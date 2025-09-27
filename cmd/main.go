1202fbe2dfe5c8a4be5c0027ff95456a8ef513b9
saiddandamaevv <saiddandamaev6@gmail.com>
2025-09-27 16:23:52 +0300

    [refactor] frontend test 2

--- cmd/main.go
+++ cmd/main.go
@@ -115,6 +115,9 @@ func main() {
 // настройка CORS для фронта
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
